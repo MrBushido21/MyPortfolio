fdb1377ad6536760aa34443431811220e2f33a53
MrBushido21 <olegliskom@gmail.com>
2023-10-14 11:29:38 +0300

    added todolist

--- src/DataProjects.ts
+++ src/DataProjects.ts
@@ -2,6 +2,10 @@ import quizImg from './img/quiz-img.png'
 import usersImg from './img/users-img.png'
 import currencyImg from './img/currency-img.png'
 import photosImg from './img/photos-img.png'
+import socialNetImg from './img/social-network.png'
+import storeImg from './img/store-app.png'
+import animeSite from './img/animeSite.png'
+import todolist from './img/todolist.png'
 
 type DataProjectsType = {
     to: string
@@ -14,5 +18,11 @@ export const dataProjects: DataProjectsType[] = [
     {to: "users", src: usersImg, title: "Users App"},
     {to: "currency", src: currencyImg, title: "Currency App"},
     {to: "photos", src: photosImg, title: "Photos App"},
+]
+export const dataProjectsLink: DataProjectsType[] = [
+    {to: "https://mrbushido21.github.io/demo-social-app", src: socialNetImg, title: "Social Network App" },
+    {to: "https://mrbushido21.github.io/store-app/", src: storeImg, title: "Stroe App"},
+    {to: "https://haki-anime-app.vercel.app", src: animeSite, title: "Anime site"},
+    {to: "https://todolist-nup8.vercel.app", src: todolist, title: "Todolist App"},
 ]
     
--- src/Pages/projects/Projects.tsx
+++ src/Pages/projects/Projects.tsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom'
 import './projects.css'
 import { Grid } from '@mui/material'
-import { dataProjects } from '../../DataProjects'
+import { dataProjects, dataProjectsLink } from '../../DataProjects'
 import socialNetImg from '../../img/social-network.png'
 import storeImg from '../../img/store-app.png'
 import animeSite from '../../img/animeSite.png'
 type PropsType = {}
 export const Projects = ({ }: PropsType) => {
 
-    let projects = dataProjects
+    let projects = dataProjects;
+    let projectsLink = dataProjectsLink;
 
     return (
         <div className='projects container'>
@@ -22,24 +23,14 @@ export const Projects = ({ }: PropsType) => {
                         <span>{project.title}</span>
                     </Grid>
                 )}
-                <Grid className='projects__card dark: bg-slate-50' item xs={12} sm={5} md={4} lg={3}>
-                    <a target='_blank' href='https://mrbushido21.github.io/demo-social-app' className="projects__nav-link">
-                        <img className='projects__img' src={socialNetImg} alt="" />
-                    </a>
-                    <span>Social Network App</span>
-                </Grid>
-                <Grid className='projects__card dark: bg-slate-50' item xs={12} sm={5} md={4} lg={3}>
-                    <a target='_blank' href='https://mrbushido21.github.io/store-app/' className="projects__nav-link">
-                        <img className='projects__img' src={storeImg} alt="" />
-                    </a>
-                    <span>Stroe App</span>
-                </Grid>
-                <Grid className='projects__card dark: bg-slate-50' item xs={12} sm={5} md={4} lg={3}>
-                    <a target='_blank' href='https://haki-anime-app.vercel.app' className="projects__nav-link">
-                        <img className='projects__img' src={animeSite} alt="" />
-                    </a>
-                    <span>Anime site</span>
-                </Grid>
+                {projectsLink.map(project =>
+                    <Grid className='projects__card dark: bg-slate-50' item xs={12} sm={5} md={4} lg={3}>
+                        <a className="projects__nav-link" target='_blank' href={project.to}>
+                            <img className='projects__img' src={project.src} alt="" />
+                        </a>
+                        <span>{project.title}</span>
+                    </Grid>
+                )}
             </Grid>
         </div>
     );
